c4ad3f077b5a530793384c27c66de36448573e45
blanquizel <blanquizel@gmail.com>
2023-02-02 16:44:17 +0800

    feature: base server code

---
+++ packages/sso/server1/app.ts
@@ -0,0 +1,13 @@
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const PORT = 3001;
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+})
+
+app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+})
