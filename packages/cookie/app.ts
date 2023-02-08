0f601e1c29f0f03b2fc18766ea9974d3e739c2a1
blanquizel <blanquizel@gmail.com>
2023-02-08 18:33:20 +0800

    feature: use express-session for cookie

--- packages/cookie/app.ts
+++ packages/cookie/app.ts
@@ -1,8 +1,21 @@
 import express, { Express, Request, Response } from 'express';
-const cookieParser = require('cookie-parser');
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
 
 const app: Express = express();
 app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const EXPIRE: number = 1000 * 60 * 5;
+
+app.use(session({
+    secret: 'secret key',
+    name: 'COOKIE DEMO',
+    cookie: { maxAge: EXPIRE },
+    resave: false,
+    saveUninitialized: false,
+}));
 
 const PORT = 3001;
 
@@ -12,43 +25,20 @@ type User = {
     session: string;
 }
 
-const EXPIRE: number = 1000 * 60 * 5;
-
 const users: User[] = [{ name: 'aaa', pw: '123', session: 'user1aaa' }, { name: 'bbb', pw: '456', session: 'user2bbb' }];
 
-const cache = new Map();
-
-const refreshCookie = (cache: Map<string, any>, sessionId: string) => {
-    let timer = cache.get(sessionId)!;
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-        cache.delete(sessionId);
-    }, EXPIRE);
-    cache.set(sessionId, timer);
-    console.log(`coookie ${sessionId} has delay ${EXPIRE}ms`)
-}
-
-const validCookie = (cookies: any): boolean => {
-    console.log('cookie:', cookies);
-    console.log('cache:', cache);
-    // warning, [Object: null prototype]
-    if (cookies && JSON.stringify(cookies) !== '{}' && cookies.hasOwnProperty('mySession')) {
-        if (cache.has(cookies['mySession'])) {
-            refreshCookie(cache, cookies['mySession']);
-            return true;
-        }
-    }
-    return false;
-}
-
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+    let session_DB: any = req.session;
+    if (session_DB.username) {
+        return res.send('Has ready login');
+    }
+    res.send('Not Login');
 });
 
 app.get('/login', (req: Request, res: Response) => {
     // check cookie
-    const cookies = req.cookies;
-    if (validCookie(cookies)) {
+    let session_DB: any = req.session;
+    if (session_DB.username) {
         return res.send('Has ready login');
     }
 
@@ -65,25 +55,16 @@ app.get('/login', (req: Request, res: Response) => {
         return res.send('Not find User or PW not right');
     }
 
-    // add session to cookie
-    res.cookie('mySession', item.session, { expires: new Date(Date.now() + EXPIRE), httpOnly: true })
-
-    // keep cookie in state and define a timer for clean
-    const timer = setTimeout(() => {
-        cache.delete(item.session);
-    }, EXPIRE);
-    cache.set(item.session, timer);
+    session_DB = req.session;
+    session_DB.username = name;
 
     return res.send('Login Success');
 })
 
-app.get('/state', (req: Request, res: Response) => {
-    const cookies = req.cookies;
-    if (validCookie(cookies)) {
-        return res.send('User has ready Login');
-    }
-    return res.send('Not Login');
-});
+app.get('/logout', (req: Request, res: Response) => {
+    req.session.destroy( () => {});
+    res.redirect('/');
+})
 
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
