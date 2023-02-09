2d8c27efedd9eed2fe7715e3a3392182cc61a1c4
blanquizel <blanquizel@gmail.com>
2023-02-09 20:29:05 +0800

    feature: use jsonwebtoken for jwt

--- packages/jwt/README.md
+++ packages/jwt/README.md
@@ -3,17 +3,21 @@ This demo is about the following points:
 
 + How to start a server written in TypeScript using Node.js and Express.
 + How to create a login system using JWT.
-+ Notice,Token is being used as a query params, it's just a demo. In your work, it would be better to set it in the Authorization header.
 
 ### ROADMAP
 
-- [x] Sever 
+- [x] Sever Start
+- [x] Login Api
+- [x] Verify Api
+- [x] Test
 
 ### How to use
 
 + `pnpm install` install dependencies
 + `pnpm run dev` run server
-+ visit `http://127.0.0.1:3001/` to test server is running
++ Use test.http file by Rest Client for test
 
 
 ### REFERNCES
+
+[JWT.IO](https://jwt.io)
--- packages/jwt/app.ts
+++ packages/jwt/app.ts
@@ -1,15 +1,27 @@
 import express, { Express, Request, Response } from 'express';
 import { createHmac } from 'node:crypto';
+import jwt from 'jsonwebtoken';
 
 const app: Express = express();
 const PORT = 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const SECRET_KEY = 'secret_key';
+const EXPIRE: number = 1000 * 60 * 5;
 
 type User = {
     name: string;
     pw: string;
 }
+
+enum TokenVerifyEnum {
+    OK,
+    INVALID,
+    EXP
+}
+
 const users: User[] = [{ name: 'aaa', pw: '123' }, { name: 'bbb', pw: '456' }];
 
 const genHeader = (): string => {
@@ -19,10 +31,12 @@ const genHeader = (): string => {
     })).toString('base64url');
 }
 const genPayload = (user: string): string => {
+    const time = new Date().getTime();
     return Buffer.from(JSON.stringify({
         "sub": "jwt-test",
         "name": user,
-        "iat": new Date().getTime(),
+        "iat": time,
+        "exp": time + EXPIRE,
     })).toString('base64url');
 };
 
@@ -31,7 +45,7 @@ const genSignature = (header: string, payload: string, secret: string): string =
         .digest('base64url');
 }
 
-const genToken = (user: User) => {
+const genToken = (user: User) => { 
     const header = genHeader();
     const payload = genPayload(user.name);
     const signature = genSignature(header, payload, SECRET_KEY);
@@ -40,43 +54,92 @@ const genToken = (user: User) => {
 }
 
 
-const validToken = (token: string) => {
+const validToken = (token: string): TokenVerifyEnum => {
     const [headerStr, payloadStr, signature] = token.split('.');
     const _signature = genSignature(headerStr, payloadStr, SECRET_KEY);
-    return signature === _signature;
+    if (signature !== _signature) {
+        return TokenVerifyEnum.INVALID;
+    }
+    return TokenVerifyEnum.OK;
 }
-
-// console.log(genToken(users[0]));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 })
 
-app.get('/login', (req: Request, res: Response) => {
-    const { name, pw } = req.query;
+app.post('/login', (req: Request, res: Response) => {
+    const { name, pw } = req.body;
+    // console.log(req.body);
     if (!name || !pw) {
-        return res.send('Missing Parameters');
+        return res.json({
+            code: 1,
+            msg: '缺少参数'
+        });
     }
     const item = users.find(item => item.name === name && item.pw === pw);
     if (!item) {
-        return res.send('Not find User or PW not right');
+        return res.json({
+            code: 1,
+            msg: '未找到指定用户'
+        })
     }
 
-    const token = genToken(item);
+    jwt.sign({ name: item.name }, SECRET_KEY, { expiresIn: '30s' }, (err, token) => {
+        return res.json({
+            code: 0,
+            token,
+        })
+    })
 
-    return res.send(`Your token is: ${token}`);
+    // const token = genToken(item);
+    // return res.json({
+    //     code: 0,
+    //     data: {
+    //         token
+    //     }
+    // })
 })
 
-app.get('/state', (req: Request, res: Response) => {
-    const { token } = req.query;
+app.get('/verify', (req: Request, res: Response) => {
+    const header = req.headers;
+    // console.log(header);
+    const token = header['authorization']?.split(' ')[1];
     if (!token) {
-        return res.send('Not find token');
+        return res.json({
+            code: 1,
+            msg: 'Not find token'
+        });
     }
-    console.log(token);
-    if (validToken(token as string)) {
-        return res.send('User has ready Login');
-    }
-    return res.send('Not Login');
+
+    return jwt.verify(token, SECRET_KEY ,(err, payload) => {
+        if(err) res.sendStatus(403);
+        return res.json({
+            code: 0,
+            msg: 'You\'re ready Login',
+            data: payload
+        })
+    })
+
+    // console.log(token);
+    // const result: TokenVerifyEnum = validToken(token as string);
+    // console.log(result);
+    // if (result === TokenVerifyEnum.INVALID) {
+    //     return res.json({
+    //         code: 1,
+    //         msg: 'Token is not pass verify'
+    //     });
+    // }
+    // if (result === TokenVerifyEnum.EXP) {
+    //     return res.json({
+    //         code: 1,
+    //         msg: 'Token is expired'
+    //     });
+    // }
+    // return res.json({
+    //     code: 0,
+    //     msg: ' You\'re ready Login',
+    //     data: JSON.parse(token.split('.')[1])
+    // })
 })
 
 app.listen(PORT, () => {
